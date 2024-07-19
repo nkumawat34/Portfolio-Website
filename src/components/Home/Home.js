16f30fd849e6e2b7c06366e17c9a4891ff0c65d9
Neeraj Kumawat <42885320+nkumawat34@users.noreply.github.com>
2024-07-19 07:12:46 -0700

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
         <Container className="home-content">
         
           <Row>
-           <h3 className="text-center"><span className="fw-bold">No.of Vistors: </span>{count}</h3>
+           <h3 className="text-center"><span className="fw-bold">No.of Visitors: </span>{count}</h3>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
